823cfcbfa9f408b5ae6fffcbaee6afd2c907ee3e
federicovazzoler <federico.vazzoler@cern.ch>
2018-11-16 18:03:25 +0100

    Added scripts to call ROOT macro

---
+++ scripts/call_root.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+set -x
+
+root -l -b -q $scriptsPATH/utility/create_nTuple_from_data.C+\(\"$treePATH\",\"$outFileNAME_today\",\"$runSummary\",\"$fillSummary\",\"$todayFileList\",$numRunAnalyze,$filesAlreadyProcessed,$files_to_process\)
+
+exit
