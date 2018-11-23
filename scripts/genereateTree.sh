4f1400b42123fced978aaff17a04f074a45de378
federicovazzoler <federico.vazzoler@cern.ch>
2018-11-23 19:07:00 +0100

    Optimization of the "call root" script (continue)

--- scripts/genereateTree.sh
+++ scripts/genereateTree.sh
@@ -2,6 +2,10 @@
 
 set -x
 
-root -l -b -q $scriptsPATH/utility/create_nTuple_from_data_NoCrystalTree.C+\(\"$treePATH\",\"$outFileNAME_today\",\"$runSummary\",\"$fillSummary\",\"$todayFileList\",$numRunAnalyze,$filesAlreadyProcessed,$files_to_process\)
+if [ "$1" ]; then
+	root -l -b -q $scriptsPATH/utility/create_nTuple_from_data_NoCrystalTree.C+\(\"$treePATH\",\"$outFileNAME_today\",\"$runSummary\",\"$fillSummary\",\"$todayFileList\",$numRunAnalyze,$filesAlreadyProcessed,$files_to_process\)
+else
+	root -l -b -q $scriptsPATH/utility/create_nTuple_from_data.C+\(\"$treePATH\",\"$outFileNAME_today\",\"$runSummary\",\"$fillSummary\",\"$todayFileList\",$numRunAnalyze,$filesAlreadyProcessed,$files_to_process\)
+fi
 
 exit
